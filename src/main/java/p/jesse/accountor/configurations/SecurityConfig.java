96db836b232856e586515c290de8c3fdc3a66d63
Jesse Plym <bgu254@myy.haaga-helia.fi>
2023-09-12 21:13:08 +0300

    updated cors

--- src/main/java/p/jesse/accountor/configurations/SecurityConfig.java
+++ src/main/java/p/jesse/accountor/configurations/SecurityConfig.java
@@ -89,7 +89,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://127.0.0.1:5500/"));
+        configuration.setAllowedOrigins(List.of("http://127.0.0.1:5500/", "http://localhost:5173/"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
